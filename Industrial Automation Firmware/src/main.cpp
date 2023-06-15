64530ce94b723b3fa3f9f2299220506dab55ed6a
anushanga pavith <anushangapavith2@gmail.com>
2023-06-15 19:33:49 +0530

    Added dual core functionality to the main code

--- Industrial Automation Firmware/src/main.cpp
+++ Industrial Automation Firmware/src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
 
+// Create 2 tasks for each processor
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
 int calibrationTime = 30;
 long unsigned int lowIn;
 unsigned long motionPause = 5000;
@@ -25,10 +29,46 @@ void setup() {
   Serial.println(" Done");
   Serial.println("SENSOR ACTIVE");
   delay(50);
+  // End of calibrating sensor
+
+  // create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+  xTaskCreatePinnedToCore(
+                    Task1code,   /* Task function. */
+                    "Task1",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task1,      /* Task handle to keep track of created task */
+                    0);          /* pin task to core 0 */                  
+  delay(500); 
+
+  //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+  xTaskCreatePinnedToCore(
+                    Task2code,   /* Task function. */
+                    "Task2",     /* name of task. */
+                    10000,       /* Stack size of task */
+                    NULL,        /* parameter of the task */
+                    1,           /* priority of the task */
+                    &Task2,      /* Task handle to keep track of created task */
+                    1);          /* pin task to core 1 */
+    delay(500);
+
 }
 
-void loop() {
-  if (digitalRead(pirPin) == HIGH) {
+
+//Task1code: blinks an LED every 1000 ms
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for(;;){
+  } 
+}
+
+//Task2code: PIR sensor
+void Task2code( void * pvParameters ){
+  for(;;) {
+    if (digitalRead(pirPin) == HIGH) {
     digitalWrite(ledPin, HIGH);   // the LED visualizes the sensor's output pin state
     if (lockLow) {
       lockLow = false;
@@ -56,4 +96,9 @@ void loop() {
       delay(50);
     }
   }
+  }
+}
+
+void loop() {
+  
 }
